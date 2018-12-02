94afde38cb1f1416e6e91d4c0ddc3afb6e70b5ac
caolu <chaolu@imudges.com>
2018-12-02 12:20:07 +0800

    3

---
+++ app/src/main/java/com/shxy/datasharedplatform/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.shxy.datasharedplatform;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shxy on 2018/12/2.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/shxy/datasharedplatform/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.shxy.datasharedplatform;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by shxy on 2018/12/2.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
